8ba4307714142c2f38d67eb721118ecfe1acc320
Harshit Jain <iharshitjain99@gmail.com>
2025-07-22 22:02:02 +0530

    created footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 [Your Name]. All rights reserved.</p>
+      <div className="social-links">
+        <a href="#">LinkedIn</a>
+        <a href="#">GitHub</a>
+        <a href="#">Twitter</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
