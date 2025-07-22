096718ba70e4f5221872e5ce613d3a318e25a652
Harshit Jain <iharshitjain99@gmail.com>
2025-07-22 22:00:37 +0530

    created basic about component

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <section id="about" className="about">
+      <h2>About Me</h2>
+      <p>
+        I'm a passionate web developer with experience in building responsive and
+        user-friendly websites using React, JavaScript, and modern frameworks.
+        Love creating clean and efficient code!
+      </p>
+    </section>
+  );
+}
+
+export default About;
