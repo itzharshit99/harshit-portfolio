970adb9390ef4095cf03467567ff20828253c89b
Harshit Jain <iharshitjain99@gmail.com>
2025-07-22 22:01:37 +0530

    created contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+function Contact() {
+  return (
+    <section id="contact" className="contact">
+      <h2>Contact Me</h2>
+      <form>
+        <input type="text" placeholder="Name" required />
+        <input type="email" placeholder="Email" required />
+        <textarea placeholder="Your Message" required></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
