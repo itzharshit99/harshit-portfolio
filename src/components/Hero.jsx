eb9a95b7d65d11b9081d41b2f7ff73c61d0fd037
Harshit Jain <iharshitjain99@gmail.com>
2025-07-22 22:00:14 +0530

    created basic hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section id="hero" className="hero">
+      <h1>Hi, I'm Harshit Jain</h1>
+      <p>Web Developer | Designer | Tech Enthusiast</p>
+      <button className="cta-button">View My Work</button>
+    </section>
+  );
+}
+
+export default Hero;
